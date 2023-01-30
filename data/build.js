b84219e5feec1b3b45bd8ce91d0f38a1a823bc55
YellowWall <ivarsig94@gmail.com>
2023-01-30 00:12:17 +0000

    smá kóði kominn

---
+++ data/build.js
@@ -0,0 +1,20 @@
+let file = read("/laeknadeild.csv")  
+
+
+
+//smíðum algjöran grunn html
+//tökum við csv skránni til að lesa úr henni
+//búum til titil á síðu
+function headmake(){
+    headdiv = document.createElement("head")
+    headdiv.innerHtml = "<head><meta charset=\"utf-8\"><Title=\"Læknadeild\"></head>"
+}
+//búum til lýsingu  og BIG TITLE á síðu
+function topofpage(){
+
+}
+//búum til lista sem heldur utan um fimm element: Númer, Heiti, Einingar, Kennslutímabil, námsstig
+function listmake(){
+    
+}
+//listi verður að vera raðanlegur, búum til menubox fyrir það
